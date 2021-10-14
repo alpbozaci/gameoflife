67b88b34cb858b0261ba59e810059d5b7c7049d1
Bozaci Alp, INI-DOS-BLG (EXT) <Alp.Bozaci@swisscom.com>
2021-10-14 15:16:35 +0200

    implementation

--- src/main/java/ch/bozaci/gameoflife/GameOfLife.java
+++ src/main/java/ch/bozaci/gameoflife/GameOfLife.java
@@ -1,18 +1,33 @@
 package ch.bozaci.gameoflife;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.shape.Line;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 
 public class GameOfLife extends Application
 {
-    private static final Integer SCREEN_WIDTH = 1500;
-    private static final Integer SCREEN_HEIGHT = 1000;
-    private static final Integer DELTA = 10;
-    
+    private static final Integer DELTA = 20;
+    private static final Integer SCREEN_WIDTH = 2000;
+    private static final Integer SCREEN_HEIGHT = 1200;
+
+    private static final Integer X_MATRIX_MAX = 100;
+    private static final Integer Y_MATRIX_Max = 50;
+
+    private GameOfLifeItem[][] matrix;
+
+    private Timeline timeline;
+
     public static void main(String[] args)
     {
         launch(args);
@@ -21,28 +36,163 @@ public class GameOfLife extends Application
     @Override
     public void start(Stage stage)
     {
-        Group root = new Group();
-        drawFields(root);
+        timeline = new Timeline(new KeyFrame(Duration.ZERO, new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent event)
+            {
+                calculateNextMatrix();
+            }
+        }), new KeyFrame(Duration.millis(100)));
 
-        Scene scene = new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT);
+        timeline.setCycleCount(Timeline.INDEFINITE);
+
+        BorderPane borderPane = new BorderPane();
+        Group matrixGrp = new Group();
+        createMatrix();
+        drawMatrix(matrixGrp);
+        borderPane.setCenter(matrixGrp);
+
+        // Group buttonGrp = new Group();
+        FlowPane buttonGrp = new FlowPane();
+        buttonGrp.getChildren().add(createNextButton());
+        buttonGrp.getChildren().add(createStartButton());
+        buttonGrp.getChildren().add(createStopButton());
+        borderPane.setBottom(buttonGrp);
+
+        Scene scene = new Scene(borderPane, SCREEN_WIDTH, SCREEN_HEIGHT);
         stage.setScene(scene);
         stage.show();
     }
-    
-    private void drawFields(Group root)
+
+    private void createMatrix()
     {
-        for (int i = 0; i < SCREEN_HEIGHT; i += DELTA)
+        matrix = new GameOfLifeItem[X_MATRIX_MAX][Y_MATRIX_Max];
+
+        for (int y = 0; y < Y_MATRIX_Max; y++)
         {
-            Line line = new Line();
-            
-            line.setStartX(0);
-            line.setStartY(i);
-            line.setEndX(SCREEN_WIDTH);
-            line.setEndY(i);
-            
-            System.out.println("line y: " + i);
-            
-            root.getChildren().add(line);
+            for (int x = 0; x < X_MATRIX_MAX; x++)
+            {
+                GameOfLifeItem item = createItem(x, y);
+                matrix[x][y] = item;
+            }
         }
     }
+
+    private void drawMatrix(Group group)
+    {
+        for (int y = 0; y < Y_MATRIX_Max; y++)
+        {
+            for (int x = 0; x < X_MATRIX_MAX; x++)
+            {
+                GameOfLifeItem rectangle = matrix[x][y];
+                group.getChildren().add(rectangle);
+            }
+        }
+    }
+
+    private void redrawMatrix()
+    {
+        for (int y = 0; y < Y_MATRIX_Max; y++)
+        {
+            for (int x = 0; x < X_MATRIX_MAX; x++)
+            {
+                GameOfLifeItem item = matrix[x][y];
+                if (item.isNextState())
+                {
+                    item.setAlive();
+                }
+                else
+                {
+                    item.setDead();
+                }
+            }
+        }
+    }
+
+    private GameOfLifeItem createItem(double x, double y)
+    {
+        GameOfLifeItem item = new GameOfLifeItem(x, y, DELTA);
+
+        item.setOnMouseClicked(new EventHandler<MouseEvent>()
+        {
+            @Override
+            public void handle(MouseEvent event)
+            {
+                System.out.println("clicked: " + item.getId());
+
+                if (item.isDead())
+                {
+                    item.setAlive();
+                }
+                else
+                {
+                    item.setDead();
+                }
+            }
+        });
+
+        return item;
+    }
+
+    private Button createNextButton()
+    {
+        Button button = new Button("next");
+        button.setOnMouseClicked(new EventHandler<MouseEvent>()
+        {
+            @Override
+            public void handle(MouseEvent event)
+            {
+                System.out.println("next");
+                calculateNextMatrix();
+            }
+        });
+
+        return button;
+    }
+
+    private Button createStartButton()
+    {
+        Button button = new Button("start");
+        button.setOnMouseClicked(new EventHandler<MouseEvent>()
+        {
+            @Override
+            public void handle(MouseEvent event)
+            {
+                System.out.println("start");
+                timeline.play();
+            }
+        });
+
+        return button;
+    }
+
+    private Button createStopButton()
+    {
+        Button button = new Button("stop");
+        button.setOnMouseClicked(new EventHandler<MouseEvent>()
+        {
+            @Override
+            public void handle(MouseEvent event)
+            {
+                System.out.println("stop");
+                timeline.stop();
+            }
+        });
+
+        return button;
+    }
+
+    private void calculateNextMatrix()
+    {
+        for (int y = 1; y < Y_MATRIX_Max - 1; y++)
+        {
+            for (int x = 1; x < X_MATRIX_MAX - 1; x++)
+            {
+                GameOfLifeService.calculateNextState(matrix, x, y);
+            }
+        }
+        redrawMatrix();
+    }
+
 }
---
+++ src/main/java/ch/bozaci/gameoflife/GameOfLifeService.java
@@ -0,0 +1,61 @@
+package ch.bozaci.gameoflife;
+
+import java.util.List;
+
+
+public class GameOfLifeService
+{
+
+    public GameOfLifeService()
+    {
+    }
+
+    public static void calculateNextState(GameOfLifeItem[][] matrix, int x, int y)
+    {
+        GameOfLifeItem itemNW = matrix[x - 1][y - 1];
+        GameOfLifeItem itemN_ = matrix[x + 0][y - 1];
+        GameOfLifeItem itemNE = matrix[x + 1][y - 1];
+
+        GameOfLifeItem itemW_ = matrix[x - 1][y];
+        GameOfLifeItem item__ = matrix[x + 0][y];
+        GameOfLifeItem itemE_ = matrix[x + 1][y];
+
+        GameOfLifeItem itemSW = matrix[x - 1][y + 1];
+        GameOfLifeItem itemS_ = matrix[x + 0][y + 1];
+        GameOfLifeItem itemSE = matrix[x + 1][y + 1];
+
+        List<GameOfLifeItem> list = List.of(itemNW, itemN_, itemNE, itemW_, itemE_, itemSW, itemS_, itemSE);
+
+        long countOfAliveItems = list.stream().filter(item -> item.isAlive()).count();
+
+        //System.out.println("x = " + x + ", y = " + y + ", count = " + countOfAliveItems);
+        
+        if (item__.isAlive())
+        {
+            if (countOfAliveItems < 2 || countOfAliveItems > 3)
+            {
+                //System.out.println("item is dead");
+                item__.setNextState(false); // will die
+            }
+            if (countOfAliveItems == 2 || countOfAliveItems == 3)
+            {
+                //System.out.println("item remains alive");
+                item__.setNextState(true);
+            }
+        }
+
+        if (item__.isDead())
+        {
+            if (countOfAliveItems == 3)
+            {
+                //System.out.println("item is born");
+                item__.setNextState(true);
+            }
+            else
+            {
+                //System.out.println("item remains dead");
+                item__.setNextState(false);
+            }
+        }
+    }
+}
